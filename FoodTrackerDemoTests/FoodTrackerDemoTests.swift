706d10d111eecf7bb3a7b0323c0b90b757810666
Dean Macaulay <deanmac5@gmail.com>
2015-12-14 22:15:58 +1100

    Initial Commit

---
+++ FoodTrackerDemoTests/FoodTrackerDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  FoodTrackerDemoTests.swift
+//  FoodTrackerDemoTests
+//
+//  Created by Dean on 14/12/2015.
+//  Copyright © 2015 Dean. All rights reserved.
+//
+
+import XCTest
+@testable import FoodTrackerDemo
+
+class FoodTrackerDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
